84133d946eb2e12b3020d8da65d87184449f0e12
saima365 <saimaislam365@gmail.com>
2025-08-13 13:03:22 +0600

    new file

---
+++ Exam/primeNumber.php
@@ -0,0 +1,50 @@
+
+ <?php
+
+if (isset($_GET['submit'])) {
+    $evenOdd=$_GET['number'];
+
+
+for ($i=0; $i < ; $i++) { 
+    
+}
+
+
+
+
+}
+?> 
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        .red{
+            color:red;
+        }
+    </style>
+</head>
+<body>
+    <form action="" method="GET">
+    <h1>Largest</h1>
+    <label for="">Give 1st Number</label>
+    <input type="text" name="number1"><br><br>
+    <label for="">Give 2nd Number</label>
+    <input type="text" name="number2"><br><br>
+    <button type="submit" name="btn_submit">Submit</button>
+    </form>
+    
+</body>
+</html>
